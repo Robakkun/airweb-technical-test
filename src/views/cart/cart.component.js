93823c4c1a440f2ae116fc071bf6a7febd189d9f
Robakkun <romain.voreaux@gmail.com>
2023-06-05 10:07:11 +0200

    add cart view + update css styles

---
+++ src/views/cart/cart.component.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import _ from 'lodash';
+
+import { Button, Divider } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import CartIcon from '@mui/icons-material/ShoppingCart';
+import { blue } from '@mui/material/colors';
+
+import CartLine from "../../components/cartLine/cart-line.component";
+
+import './cart.styles.css';
+import routes from "../../constants/routes";
+
+const Cart = () => {
+    const cartState = useSelector((state) => state.cartReducer.cartList);
+    const [productsInCartList, setProductsInCartList] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!_.isEmpty(cartState)) {
+            setProductsInCartList(cartState);
+        }
+    }, [cartState])
+
+    const getCartList = () => {
+        let uniqueKey = 0;
+        return _.map(productsInCartList, product => {
+            uniqueKey += 1;
+            return (
+                <div key={uniqueKey}>
+                    <CartLine label={product.label} price={product.price} />
+                </div>
+            )
+        })
+    }
+
+    const getTotal = () => {
+        let totalPrice = 0;
+        _.forEach(productsInCartList, product => {
+            totalPrice += product.price;
+        })
+
+        return (
+            <>
+                <div className="cart_price">
+                    <CartLine label="Total" price={totalPrice} />
+                </div>
+            </>
+        )
+    }
+
+    const returnToCatalogue = () => {
+        navigate(routes.Catalogue);
+    }
+
+    return (
+        <div className="cart_container">
+            <div className="cart_title">
+                <ArrowBackIcon onClick={() => returnToCatalogue()} />
+                <CartIcon style={{ color: blue[500] }} />
+                Your cart
+            </div>
+            <Divider />
+            {_.isEmpty(productsInCartList) &&
+                <div>
+                    There is no product in your cart
+                </div>
+            }
+            {!_.isEmpty(productsInCartList) &&
+                <>
+
+                    <div>{getCartList()}</div>
+                    <div>{getTotal()}</div>
+                    <Button />
+                </>
+            }
+        </div>
+    );
+}
+
+export default Cart;
